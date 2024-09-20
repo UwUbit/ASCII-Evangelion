be36304eeaa384d44bf7c9357e343e62c850fe1b
olezhkoistaken <mruchko13o@gmail.com>
2024-09-20 15:34:44 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from ImageConverting import get_list
 from CreateDirectoryWithFrames import get_frames_from_video
 
 
-if __name__ == "__main__":
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def main():
     keyboard = Controller()
     confirm_full_screen = input("Play in full screen? Y/n: ")
     if confirm_full_screen == 'Y':
@@ -20,34 +24,32 @@ if __name__ == "__main__":
 
     confirm_sound = input("Play with sound? Y/n: ")
 
-    # getting list with names of frames
-    directory = f"{os.path.dirname(os.path.abspath(__file__))}\\frames"
-    list_with_frame_names = []
-    for filename in os.listdir(directory):
-        if filename.endswith(".jpg"):
-            list_with_frame_names.append([filename, int(filename[0:-4])])
+    # Get list of frames
+    directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "frames")
+    list_with_frame_names = sorted(
+        [(filename, int(filename[:-4])) for filename in os.listdir(directory) if filename.endswith(".jpg")],
+        key=lambda x: x[1]
+    )
 
-    # list sorting in increasing order
-    list_with_frame_names = sorted(list_with_frame_names, key=lambda x: x[1])
-
-    # calculating output
-    out_list = []
+    # Get terminal size and calculate output frames
     columns, rows = os.get_terminal_size()
     print("Calculating output...")
-    for i in list_with_frame_names:
-        out_list.append(get_list("frames/" + i[0], columns, rows))
+    out_list = [get_list(os.path.join(directory, f[0]), columns, rows) for f in list_with_frame_names]
     print("Done!")
     time.sleep(1)
 
     if confirm_sound == 'Y':
-        audio_file = f"{os.path.dirname(os.path.abspath(__file__))}\\lost_soul_audio.mp3"
+        audio_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "lost_soul_audio.mp3")
         playsound(audio_file, False)
 
-    FPS = 30
-    timer = fpstimer.FPSTimer(FPS)
-    num_of_frames = len(list_with_frame_names)
-    for i in range(num_of_frames):
-        print(out_list.pop(0), end='')
+    # Display frames with FPS control
+    fps = 30
+    timer = fpstimer.FPSTimer(fps)
+    for frame in out_list:
+        clear_console()  # Clear screen before displaying new frame
+        print(frame, end='')  # Display current frame
         timer.sleep()
 
-    time.sleep(5)
+
+if __name__ == "__main__":
+    main()
