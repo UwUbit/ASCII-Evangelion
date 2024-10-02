c889edf162146aca5e13b977802e2e18f11e626d
Oleh Mruchko <crabinaryx@proton.me>
2024-10-02 23:12:30 +0300

    EVA Update

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import time
 import os
-import fpstimer
 from playsound import playsound
 from pynput.keyboard import Key, Controller
 from ImageConverting import get_list
 from CreateDirectoryWithFrames import get_frames_from_video
-
 
 def main():
     keyboard = Controller()
@@ -20,14 +18,12 @@ def main():
 
     confirm_sound = input("Play with sound? Y/n: ")
 
-    # Getting the list of frames
     directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "frames")
     list_with_frame_names = sorted(
         [(filename, int(filename[:-4])) for filename in os.listdir(directory) if filename.endswith(".jpg")],
         key=lambda x: x[1]
     )
 
-    # Getting terminal size and calculating output frames
     columns, rows = os.get_terminal_size()
     print("Calculating output...")
     out_list = [get_list(os.path.join(directory, f[0]), columns, rows) for f in list_with_frame_names]
@@ -35,32 +31,29 @@ def main():
     time.sleep(1)
 
     if confirm_sound == 'Y':
-        audio_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "lost_soul_audio.mp3")
+        audio_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "nge_op.mp3")
         playsound(audio_file, False)
 
-    # Displaying frames with FPS control
-    fps = 90
-    timer = fpstimer.FPSTimer(fps)
+    start_time = time.time()
 
-    # Initializing an empty previous frame for comparison
     previous_frame = [''] * rows
 
-    # Loop through each frame
-    for current_frame in out_list:
-        # Split current frame into lines
+    for frame_index, current_frame in enumerate(out_list):
+        frame_start_time = time.time()
+
         current_frame_lines = current_frame.splitlines()
 
-        # Compare and print only the lines that have changed
         for i, (old_line, new_line) in enumerate(zip(previous_frame, current_frame_lines)):
             if old_line != new_line:
-                # Move cursor to the correct line and print the new line
                 print(f"\033[{i+1}0H{new_line}")
 
-        # Update the previous frame with the current frame's data
         previous_frame = current_frame_lines.copy()
 
-        # Control FPS
-        timer.sleep()
+        expected_frame_duration = (frame_index + 1) / 30.0
+        elapsed_time = time.time() - start_time
+
+        if elapsed_time < expected_frame_duration:
+            time.sleep(expected_frame_duration - elapsed_time)
 
 
 if __name__ == "__main__":
